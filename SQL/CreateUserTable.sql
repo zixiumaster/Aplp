6deae2c7159064a3698d96cee98412838869bf43
zixiu <2396696213@qq.com>
2021-04-23 18:44:21 +0800

    v4.4.7.1 Test gitHub

---
+++ SQL/CreateUserTable.sql
@@ -0,0 +1,31 @@
+-- 建库
+CREATE DATABASE aplp;
+
+use aplp;
+
+-- 建表，学生
+CREATE TABLE user_student(
+	id VARCHAR(50) NULL UNIQUE,
+	name VARCHAR(50) NULL,
+	phone	VARCHAR(50) NULL UNIQUE,
+	email VARCHAR(50) NULL UNIQUE,
+	password VARCHAR(50) NOT NULL
+);
+
+-- 建表，教师
+CREATE TABLE user_teacher(
+	id VARCHAR(50) NULL UNIQUE,
+	name VARCHAR(50) NULL,
+	phone	VARCHAR(50) NULL UNIQUE,
+	email VARCHAR(50) NULL UNIQUE,
+	password VARCHAR(50) NOT NULL
+);
+
+-- 建表，管理员
+CREATE TABLE user_admin(
+	id VARCHAR(50) NULL UNIQUE,
+	name VARCHAR(50) NULL,
+	phone	VARCHAR(50) NULL UNIQUE,
+	email VARCHAR(50) NULL UNIQUE,
+	password VARCHAR(50) NOT NULL
+);
---
+++ src/main/java/xyz/zixiu/aplp/Service/DB/Interface/DBService.java
@@ -0,0 +1,14 @@
+package xyz.zixiu.aplp.Service.DB.Interface;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DBService {
+
+    public Boolean rebuild();
+
+    public Boolean build();
+
+    public Boolean chack();
+
+}
