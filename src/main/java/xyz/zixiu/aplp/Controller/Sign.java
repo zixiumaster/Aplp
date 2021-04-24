3f139e9e506390e3c3265b3762870de9b47c3089
zixiu <2396696213@qq.com>
2021-04-24 13:01:12 +0800

    v4.8.1 update .gitignore

--- src/main/java/xyz/zixiu/aplp/Controller/Sign.java
+++ src/main/java/xyz/zixiu/aplp/Controller/Sign.java
@@ -1,16 +1,10 @@
 package xyz.zixiu.aplp.Controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import xyz.zixiu.aplp.Bean.User.SignBean;
-import xyz.zixiu.aplp.Service.User.Impl.StudentServiceImpl;
-import xyz.zixiu.aplp.Service.User.Interface.AdminService;
-import xyz.zixiu.aplp.Service.User.Interface.StudentService;
-import xyz.zixiu.aplp.Service.User.Interface.TeacherService;
 import xyz.zixiu.aplp.Service.User.UserService;
 
 import javax.annotation.Resource;
